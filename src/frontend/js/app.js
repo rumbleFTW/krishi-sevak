090bc969ba19ec2a43c07e4340418d223a065a14
SubhojeetDas1107 <hbksubhojeetdas1107@gmail.com>
2023-02-05 13:55:04 +0530

    added frontend

---
+++ src/frontend/js/app.js
@@ -0,0 +1,21 @@
+// CHECK IF BROWSER SUPPORTS GEOLOCATION
+if('geolocation' in navigator){
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+}else{
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = "<p>Browser doesn't Support Geolocation</p>";
+}
+
+// SET USER'S POSITION
+function setPosition(position){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    alert("Your Latitude: " + latitude + ", Longitude: " + longitude);
+}
+
+// SHOW ERROR WHEN THERE IS AN ISSUE WITH GEOLOCATION SERVICE
+function showError(error){
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = `<p> ${error.message} </p>`;
+}
